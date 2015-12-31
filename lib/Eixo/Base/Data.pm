32681bf84ad944c15f947c75ac9c8af3797691f1
frmadem <frmadem@gmail.com>
2015-12-31 14:42:48 +0000

    Added helper for DATA sections

--- lib/Eixo/Base/Data.pm
+++ lib/Eixo/Base/Data.pm
@@ -3,6 +3,26 @@ package Eixo::Base::Data;
 use strict;
 use Eixo::Base::Clase;
 
+sub getDataBySections{
+    my ($module) = @_;
+
+    my $data = &getData($module);
+
+    my $sections = {};
+    my $section_name = undef;
+
+    foreach my $l (split(/\n/, $data)){
+
+        if($l =~ /^\s*\@\@(\w+)/){
+            $section_name = $1;
+        }
+        else{
+            $sections->{$section_name} .= "$l\n" if($section_name);
+        }
+    }
+
+    $sections;
+}
 
 sub getData{
 	my ($module) = @_;
